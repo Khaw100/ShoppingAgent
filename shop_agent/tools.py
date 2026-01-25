8519b2d30d2068c43c704503fc4306e3ac9f3870
Khaw100 <rakhawiratama10@gmail.com>
2026-01-26 02:22:44 +0700

    Fixing function seacrh by  materials and update prompt

--- shop_agent/tools.py
+++ shop_agent/tools.py
@@ -1,7 +1,6 @@
-from util.mongodb import get_query_results, get_item_by_product_id, get_item_by_product_by_brand, get_item_by_material
-from google.adk.tools import function_tool
+from util.mongodb import get_query_results, get_item_by_product_id, get_item_by_product_by_brand, get_item_by_material, create_transaction, get_user_transactions
+# from google.adk.runtime import get_current_context
 
-@function_tool
 def search_items(search_query: str):
     """Search items from vector DB by text query
     
@@ -10,17 +9,12 @@ def search_items(search_query: str):
     Returns:
         context (str): Returned context from Retriever
     """
-    if not search_query or not search_query.strip():
-        raise RuntimeError("search_query is empty")
     try:
         results = get_query_results(search_query)
-
-        if not results:
-            return []
-        return results
+        return {"results": results}
     except Exception as e:
-        raise RuntimeError(f"search_items failed: {str(e)}")
-@function_tool
+        return {"error" : f"There was a problem while returning the tool: {str(e)}"}
+    
 def search_item_by_id(product_id: str):
     """Get item from DB by product_id 
     
@@ -29,19 +23,12 @@ def search_item_by_id(product_id: str):
     Returns:
         context (str): Returned context from DB 
     """ 
-    if not product_id or not product_id.strip():
-        raise RuntimeError("product_id is empty")
-    
     try:
-        results =  get_item_by_product_id(product_id)
-        if not results:
-                return []
-        return results
-    
+        results = get_item_by_product_id(product_id)
+        return {"results": results}
     except Exception as e:
-        raise RuntimeError(f"search_items failed: {str(e)}")
-    
-@function_tool      
+        return {"error":f"There was a problem while returning the tool: {str(e)}"}
+        
 def search_item_by_brand(brand: str):
     """Get item from DB by brand 
     
@@ -50,21 +37,12 @@ def search_item_by_brand(brand: str):
     Returns:
         context (str): Returned context from DB 
     """ 
-    if not brand or not brand.strip():
-        raise RuntimeError("brand is empty")
-    
     try:
         results = get_item_by_product_by_brand(brand)
-
-        if not results:
-            return []
-
-        return results
-    
+        return {"results": results}
     except Exception as e:
-        raise RuntimeError(f"search_items failed: {str(e)}") 
+        return {"error":f"There was a problem while returning the tool: {str(e)}"} 
 
-@function_tool
 def search_item_by_material(material: str):
     if not material or not material.strip():
         raise RuntimeError("material is empty")
@@ -75,7 +53,62 @@ def search_item_by_material(material: str):
         if not results:
             return []
 
-        return results
+        return {"results": results}
 
     except Exception as e:
-        raise RuntimeError(f"search_item_by_material failed: {str(e)}")
+        return {"error":f"search_item_by_material failed: {str(e)}"}
+    
+
+def buy_item(product_id: str):
+    try:
+        # ctx = get_current_context()
+        user_id = "user"
+        items = get_item_by_product_id(product_id)
+
+        if not items:
+            return {"error": "Product not found"}
+
+        product = items[0]
+
+        trx = create_transaction(
+            user_id=user_id,
+            product={
+                "product_id": product_id,
+                "name": product["name"],
+                "price": product["price"]
+            }
+        )
+
+        return {
+            "final": True,
+            "text": f"Purchase successful!\n\n"
+                    f"Item: {trx['product_name']}\n"
+                    f"Price: ${trx['price']} {trx['currency']}\n"
+                    f"Order time: {trx['created_at']}"
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
+
+def get_my_transactions():
+    user_id = "user"
+    trxs = get_user_transactions(user_id)
+
+    if not trxs:
+        return {
+            "final": True,
+            "text": "🛒 You have no transactions yet."
+        }
+
+    lines = []
+    for trx in trxs:
+        lines.append(
+            f"- {trx['product_name']} | "
+            f"${trx['price']} {trx['currency']} | "
+            f"{trx['created_at']}"
+        )
+
+    return {
+        "final": True,
+        "text": "📜 Your transaction history:\n" + "\n".join(lines)
+    }
--- util/mongodb.py
+++ util/mongodb.py
@@ -1,13 +1,8 @@
 from pymongo import MongoClient
 from .embedding import get_embedding
 import os
-from urllib.parse import quote_plus
-import certifi
-
-from dotenv import load_dotenv
-load_dotenv()
-
-
+from datetime import datetime
+import uuid
 CLUSTER_HOST = "cluster0.bdvqb9v.mongodb.net/?appName=Cluster0"
 
 db_username = os.getenv('db_username')
@@ -16,18 +11,19 @@ db_password = os.getenv('db_password')
 CONNECTION_STRING = f"mongodb+srv://{db_username}:{db_password}@{CLUSTER_HOST}"
 
 # Connect to your MongoDB deployment
-client = MongoClient(CONNECTION_STRING,
-                    tls=True,
-                    tlsCAFile=certifi.where(),
-                    serverSelectionTimeoutMS=5000)
+client = MongoClient(CONNECTION_STRING)
 collection = client["rag_db"]["rag"]
+
+# Add new table
+db = client["rag_db"]
+trx_collection = db["transactions"]
 
 # Define a function to run vector search queries
 def get_query_results(query):
   """Gets results from a vector search query."""
 
   query_embedding = get_embedding(query)
-  print("QUERY EMBEDDING:", len(query_embedding))
+  print("QUERY EMBEDDING:", query_embedding)
   pipeline = [
       {
             "$vectorSearch": {
@@ -35,7 +31,6 @@ def get_query_results(query):
               "queryVector": query_embedding,
               "path": "embedding",
               "exact": True,
-              "numCandidates": 100, 
               "limit": 5
             }
       }, {
@@ -54,7 +49,8 @@ def get_query_results(query):
   return products
 
 def get_item_by_product_id(product_id):
-    results = collection.find({"product_id": product_id})
+    results = collection.find({"product_id": product_id},
+                              {"_id": 0, "name": 1, "price": 1, "product_id": 1})
     
     products = []
     for product in results:
@@ -69,7 +65,6 @@ def get_item_by_product_by_brand(brand):
         products.append(product['text'])
     return products
 
-
 def get_item_by_material(material: str):
     results = collection.find(
         {"material": {"$regex": material, "$options": "i"}}
@@ -79,3 +74,24 @@ def get_item_by_material(material: str):
     for product in results:
         products.append(product.get("text"))
     return products
+
+def create_transaction(user_id: str, product: dict):
+    trx = {
+        "transaction_id": str(uuid.uuid4()),
+        "user_id": user_id,
+        "product_id": product["product_id"],
+        "product_name": product["name"],
+        "price": product["price"],
+        "currency": "USD",
+        "created_at": datetime.utcnow()
+    }
+    trx_collection.insert_one(trx)
+    return trx
+
+def get_user_transactions(user_id: str):
+    results = trx_collection.find(
+        {"user_id": user_id},
+        {"_id": 0}
+    ).sort("created_at", -1)
+
+    return list(results)
